81399e73f2e761047f81625116ac699212056038
STACIA <69411527+staciax@users.noreply.github.com>
2023-08-08 17:29:15 +0700

    [readme] missing long_description_content_type

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     license='MIT',
     description='An Asynchronous Unofficial Valorant API Wrapper for Python',
     long_description=readme,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=requirements,
     extras_require=extras_require,
