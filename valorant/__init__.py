3bf9e3e462edf8ebd6a1c64c997ba68e695513b6
STACIA <69411527+staciax@users.noreply.github.com>
2023-08-17 19:20:15 +0700

    bump v1.0.6

--- valorant/__init__.py
+++ valorant/__init__.py
@@ -10,7 +10,7 @@ __title__ = 'valorant'
 __author__ = 'STACiA'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023-present STACiA'
-__version__ = '1.0.6a'
+__version__ = '1.0.6'
 
 from typing import Literal, NamedTuple
 
@@ -29,6 +29,6 @@ class VersionInfo(NamedTuple):
     release: Literal["alpha", "beta", "final"]
 
 
-version_info: VersionInfo = VersionInfo(major=1, minor=0, micro=6, release='alpha')
+version_info: VersionInfo = VersionInfo(major=1, minor=0, micro=6, release='final')
 
 del NamedTuple, Literal, VersionInfo
