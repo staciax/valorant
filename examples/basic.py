abaeadd985838df3cf12c249237d60e1b9f242a8
STACiA <69411527+staciax@users.noreply.github.com>
2024-12-30 12:57:51 +0700

    update example basic

--- examples/basic.py
+++ examples/basic.py
@@ -9,27 +9,30 @@ async def main() -> None:
         weapon = await client.fetch_weapon('9c82e19d-4575-0200-1a81-3eacf00cf872')  # Vandal
         assert weapon is not None
 
-        for skin in weapon.skins:
-            print(skin.display_name)  # default locale
+        for skin in weapon.skins[:5]:
+            print(skin.display_name)
             print(skin.display_icon)
 
-            # specify locale
+            # display_name locale
             print(skin.display_name.ja_JP)
             print(skin.display_name.japanese)
-            # print(skin.display_name.from_locale(valorant.Locale.japanese))
 
-            # if skin.theme is not None:
-            #     print(skin.theme.display_name)
-            #     print(skin.theme.display_icon)
+            skin_theme = await skin.fetch_theme(client=client)
+            if skin_theme is not None:
+                print(skin_theme.display_name)
+                print(skin_theme.display_icon)
 
-            # if skin.content_tier is not None:
-            #     print(skin.content_tier.display_name)
-            #     print(skin.content_tier.display_icon)
+            skin_content_tier = await skin.fetch_content_tier(client=client)
+            if skin_content_tier is not None:
+                print(skin_content_tier.display_name)
+                print(skin_content_tier.display_icon)
 
             for level in skin.levels:
                 print(level.display_name)
             for chroma in skin.chromas:
                 print(chroma.display_name)
+
+            print('-' * 40)
 
 
 asyncio.run(main())
