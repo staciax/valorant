7388dc7c11a168cd630035de031a9fcf0f6ad9b1
STACiA <69411527+staciax@users.noreply.github.com>
2025-01-01 01:24:50 +0700

    Bump version to 2.0.0

--- valorant/__init__.py
+++ valorant/__init__.py
@@ -10,7 +10,7 @@ __title__ = 'valorant'
 __author__ = 'STACiA'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023-present STACiA'
-__version__ = '2.0.0a'
+__version__ = '2.0.0'
 
 from . import models
 from .client import Client
