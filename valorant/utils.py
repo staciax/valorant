bd152c8a7aaef2ca7d77b9a6288729a87ba11bf2
STACiA <69411527+staciax@users.noreply.github.com>
2025-01-04 23:06:13 +0700

    fix: lint, test

--- valorant/utils.py
+++ valorant/utils.py
@@ -1,12 +1,14 @@
+# mypy: disable-error-code="assignment, import-not-found"
+
 import json
 
 try:
-    import msgspec  # type: ignore[import-not-found]
+    import msgspec
 except ImportError:  # pragma: no cover
     msgspec = None
 
 
-if msgspec is None:  # noqa: SIM108
+if msgspec is None:  # pragma: no cover  # noqa: SIM108
     _from_json = json.loads
 else:
     _from_json = msgspec.json.decode
