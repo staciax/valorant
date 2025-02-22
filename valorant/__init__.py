aaec0a042c677de6550314758c32f90b926cf868
STACiA <69411527+staciax@users.noreply.github.com>
2025-02-23 01:52:22 +0700

    Bump 2.0.3 for valorant patch 10.03

--- valorant/__init__.py
+++ valorant/__init__.py
@@ -10,7 +10,7 @@ __title__ = 'valorant'
 __author__ = 'STACiA'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023-present STACiA'
-__version__ = '2.0.2a'
+__version__ = '2.0.3'
 
 from . import models, utils
 from .client import Client
